7cb4b50bf5b77911b46fa2bcbb2ddd4450292413
moisgames <VLAD981010@gmail.com>
2024-09-03 23:26:53 +0700

    feat Create Route UserPage

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UserPageComponent } from './common-ui/user-page/user-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '/userList', component: UserPageComponent},
+    
+];
