a0c2430756c6f5b98f4d4d56c18b5facb675da2f
moisgames <VLAD981010@gmail.com>
2024-09-04 22:47:53 +0700

    feat List Resources Ending

--- client/src/app/common-ui/list-resources/list-resources/list-resources.component.ts
+++ client/src/app/common-ui/list-resources/list-resources/list-resources.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-list-resources',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './list-resources.component.css'
 })
 export class ListResourcesComponent {
-
+  @Input() resource!: any
 }
