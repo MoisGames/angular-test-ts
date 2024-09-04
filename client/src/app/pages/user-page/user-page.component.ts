82e108abb192cd3cf516e15c69d578a827b28a54
moisgames <VLAD981010@gmail.com>
2024-09-04 22:22:38 +0700

    chore Subscribed to change data from Resource service. And assigned a resource to the variable in User page.

--- client/src/app/pages/user-page/user-page.component.ts
+++ client/src/app/pages/user-page/user-page.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { ProfileCardComponent } from "../../common-ui/profile-card/profile-card.component";
 import { ProfileService } from '../../data/services/profile.service';
 import { ListResourcesComponent } from '../../common-ui/list-resources/list-resources/list-resources.component';
+import { ResourcesService } from '../../data/services/resources.service';
 
 
 @Component({
@@ -15,10 +16,18 @@ export class UserPageComponent {
   profileService = inject(ProfileService)
   profiles: any = []
 
+  resourceService = inject(ResourcesService)
+  resources: any = []
+
+
   constructor () {
     this.profileService.getTestAccounts()
       .subscribe(val => {
         return this.profiles = val
+      })
+    this.resourceService.getResources()
+      .subscribe(val => {
+        return this.resources = val
       })
   }
 }
