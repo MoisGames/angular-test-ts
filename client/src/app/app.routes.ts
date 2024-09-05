55b863f73b8783a3ddf2c4dd15d1c49771043afa
moisgames <VLAD981010@gmail.com>
2024-09-05 15:20:05 +0700

    chore Set up a redirect in the route and set the home page.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
 import { UserPageComponent } from './pages/user-page/user-page.component';
 import { UserCardPageComponent } from './pages/user-card-page/user-card-page.component';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+
 
 export const routes: Routes = [
     {path: 'userlist', component: UserPageComponent },
-    {path: '', component: UserPageComponent  },
+    {path: '', redirectTo: 'userlist', pathMatch: 'full' },
     {path: 'users/2', component: UserCardPageComponent},
     { path: "**", redirectTo: "/"},
 ];
