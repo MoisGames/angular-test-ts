454d4b5b23abb2b4da52b2bf712a6ff5ee53b343
moisgames <VLAD981010@gmail.com>
2024-12-03 23:59:41 +0700

    chore (pages/auth-register-page) Добавил новый компонент регистрации, авторизации

---
+++ src/app/pages/auth-register-page/auth-register-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-register-page',
+  imports: [],
+  templateUrl: './auth-register-page.component.html',
+  styleUrl: './auth-register-page.component.css'
+})
+export class AuthRegisterPageComponent {
+
+}
