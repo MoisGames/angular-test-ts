824b9806fa965227d4505f928d1768d74e77882d
moisgames <VLAD981010@gmail.com>
2024-12-04 08:02:19 +0700

    chore (app.routes.ts) Добавил новый маршрут --login--

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { UserPageComponent } from './pages/user-page/user-page.component';
 import { UserCardPageComponent } from './pages/user-card-page/user-card-page.component';
 import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
+import { AuthRegisterPageComponent } from './pages/auth-register-page/auth-register-page.component';
 
 
 export const routes: Routes = [
@@ -10,4 +11,5 @@ export const routes: Routes = [
     {path: 'users/:id', component: UserCardPageComponent, title: "Страница пользователя"},
     {path: 'settings', component: SettingsPageComponent, title: "Страница изменения данных о пользователе"},
     {path: "**", redirectTo: "users"},
+    {path:"login", component: AuthRegisterPageComponent, title: "Страница Авторизации"}
 ];
